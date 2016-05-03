eb2233746c8b8510046d66ca7900602487de7bad
chessrookie <comander@10.3.114.131>
2016-05-03 12:05:07 +0200

    Initial Gamestate

---
+++ src/ch/hslu/ta/prg2/Gamestate/Position.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.ta.prg2.Gamestate;
+
+/**
+ *
+ * 
+ */
+public class Position {
+    
+    private int x;
+    private int y;
+    
+}
---
+++ src/ch/hslu/ta/prg2/Gamestate/Ship.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.ta.prg2.Gamestate;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * 
+ */
+public class Ship {
+    
+    private ArrayList<Position> positions;
+    
+}
