62cb4a19fa5166b45195e24ce972be547247b48f
Luca Dias <luca.dias@hotmail.ch>
2016-05-09 11:38:52 +0200

    commit didn't update all files on the repo

--- src/ch/hslu/ta/prg2/view/mainframe.java
+++ src/ch/hslu/ta/prg2/view/mainframe.java
@@ -5,25 +5,35 @@ import javax.swing.*;
 /**
  *
  */
-public class mainframe extends JFrame {
+public class MainFrame extends JFrame {
 
     //Constants
     private static final int WINDOW_HEIGHT = 800;
     private static final int WINDOW_LENGTH = 1200;
 
-    public mainframe() {
+    public MainFrame() {
 
         //CREATE STARTPANEL
-        startpanel startpanel = new startpanel(WINDOW_HEIGHT,WINDOW_LENGTH);
+        StartPanel startpanel = new StartPanel(WINDOW_HEIGHT, WINDOW_LENGTH);
 
-        
-        //ADD THINGS
-        this.add(startpanel);
-       
-        //SET OPTIONS
+        setOptions();
+
+        addObjects(startpanel);
+    }
+
+    //SET OPTIONS
+    private void setOptions() {
+
         this.setBounds(100, 100, WINDOW_LENGTH, WINDOW_HEIGHT);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    //ADD THINGS
+    private void addObjects(StartPanel startpanel) {
+
+        this.add(startpanel);
 
     }
 
--- src/ch/hslu/ta/prg2/view/startpanel.java
+++ src/ch/hslu/ta/prg2/view/startpanel.java
@@ -1,41 +1,53 @@
 package ch.hslu.ta.prg2.view;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.*;
-
 
 /**
  *
  */
+public class StartPanel extends JPanel {
 
-
-public class startpanel extends JPanel {
-    
     //COMPONENTS
     JButton btn_start;
-    
-    
-    
-//    public startpanel(){
-//    
-//        this(mainframe.WIDTH,mainframe.HEIGHT);
-//    }
-//    
-    
-    public startpanel(int WINDOW_HEIGHT,int WINDOW_LENGTH){
-        
+    JLabel lbl_declareName;
+    JTextField txt_nameField;
+
+    public StartPanel(int WINDOW_HEIGHT, int WINDOW_LENGTH) {
+
         this.setSize(WINDOW_HEIGHT, WINDOW_LENGTH);
-        
+
         createComponents();
-        
-        
+
+        addObjects();
+        setOptions();
+
+    }
+
+    //CREATE OBJECTS
+    private void createComponents() {
+
+        lbl_declareName = new JLabel("Bitte gib deinen Namen an:");
+        txt_nameField = new JTextField();
+        btn_start = new JButton("Start");
+
+    }
+
+    //SET OPTIONS        
+    private void setOptions() {
+        this.setLayout(new BorderLayout());
+
+//        lbl_declareName.setBounds(800, 800, 100, 50);
+//        txt_nameField.setBounds(0, 0, 100, 100);
+//        btn_start.setBounds(400, 400, 1000, 1000);
+    }
+
+    private void addObjects() {
+        this.add(lbl_declareName);
+        this.add(txt_nameField);
         this.add(btn_start);
+
     }
-    
-    private void createComponents(){
-        
-        btn_start = new JButton();
-        btn_start.setText("HALLO MAMI");
-        btn_start.setBounds(400, 400, 1000, 1000);
-        
-    }
+
 }
