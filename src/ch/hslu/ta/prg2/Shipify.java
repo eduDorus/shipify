50059ecb4ba8e53bd22f5e330a75f05d2a37c6ee
disperate <julian.bigler@hotmail.com>
2016-05-25 11:14:17 +0200

    Format

--- src/ch/hslu/ta/prg2/Shipify.java
+++ src/ch/hslu/ta/prg2/Shipify.java
@@ -1,33 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.hslu.ta.prg2;
 
 import ch.hslu.ta.prg2.gui.GUIController;
 
-/**
- *
- * @author julian
- */
 public class Shipify {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
 
-        //GUI SUPER CONTROLLER
-        //=================
         GUIController superController = new GUIController();
-
         
-        
-        
-
     }
-
-
 }
